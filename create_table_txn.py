7bf8e62c45e122e09ab0b80050f981778a36ed5e
spiderflying <spiderflying@gmail.com>
2021-09-19 22:05:05 -0700

    update create_table_txn.py

--- create_table_txn.py
+++ create_table_txn.py
@@ -1,5 +1,5 @@
 from google.cloud import bigquery
-from google.cloud.exceptions import NotFound
+# from google.cloud.exceptions import NotFound
 
 
 PROJECT_ID = 'datapipeline-325719'
@@ -10,9 +10,17 @@ table_name='txn_by_address'
 
 client = bigquery.Client(project=PROJECT_ID, location="US")
 
+# tell the client everything it needs to know to upload our csv
+dataset_ref = client.dataset(dataset_id)
+table_ref = dataset_ref.table(table_name)
+
+
 try:
     client.get_table(table_id)  # Make an API request.
     print("Table {} already exists.".format(table_id))
+    previous_rows = client.get_table(table_ref).num_rows
+    print("previous_rows {}".format(previous_rows))
+
 except:
     pass
     print("Table {} is not found.".format(table_id))
@@ -29,28 +37,38 @@ except:
         bigquery.SchemaField("send", "INTEGER"),
         bigquery.SchemaField("receive", "INTEGER"),
         bigquery.SchemaField("confirmations", "INTEGER"),
-        bigquery.SchemaField("confirmed", "INTEGER")
-
+        bigquery.SchemaField("confirmed", "INTEGER"),
+        bigquery.SchemaField("outflow", "FLOAT"),
+        bigquery.SchemaField("inflow", "FLOAT"),
+        bigquery.SchemaField("netflow", "FLOAT")
+      #  bigquery.SchemaField("balance", "FLOAT", mode="REQUIRED")
     ]
-
-
 
     table = bigquery.Table(table_id, schema=schema)
     table = client.create_table(table)
     print("Created table {}".format(table_id))
 
-# tell the client everything it needs to know to upload our csv
-dataset_ref = client.dataset(dataset_id)
-table_ref = dataset_ref.table(table_name)
+
 job_config = bigquery.LoadJobConfig()
 job_config.source_format = bigquery.SourceFormat.CSV
 job_config.skip_leading_rows = 1
+job_config.write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE
 
-# load the csv into bigquery
-with open(filename, "rb") as source_file:
-    job = client.load_table_from_file(source_file, table_ref, job_config=job_config)
+#load data from cloud storage
+uri = "gs://dp-etherscan/get_txn_by_address.csv"
+load_job = client.load_table_from_uri(
+    uri, table_id, job_config=job_config
+)  # Make an API request.
 
-job.result()
+
+# # load the csv into bigquery
+# with open(filename, "rb") as source_file:
+#     job = client.load_table_from_file(source_file, table_ref, job_config=job_config)
+
+load_job.result()
 
 # looks like everything worked :)
-print("Loaded {} rows into {}.".format(job.output_rows, table_id))
+print("Loaded {} rows into {}.".format(load_job.output_rows, table_id))
+
+after_rows = client.get_table(table_ref).num_rows
+print("after_rows {}".format(after_rows))
